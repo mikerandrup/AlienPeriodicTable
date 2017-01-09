d12cef6890c0b9772d919a107172d9b339439388
Mike Randrup <github@mikerandrup.com>
2017-01-09 14:38:26 -0600

    Completes #1.  Adding initial scaffolding of the MVC App to source control.

---
+++ SearchTiles/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace SearchTiles.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("~/Views/ReactApplication.cshtml");
+        }
+    }
+}
